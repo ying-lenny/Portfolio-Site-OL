f8f9b1a43203451f32d7b239cc58bcc823c164e9
Owen Lenihan <owenlenihan101@gmail.com>
2023-04-18 10:51:21 +0100

    Update Projects.js

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -3,14 +3,13 @@ import React from "react";
 export default function ProjectSection() {
     return (
         <div className="project-section">
-            <h2>This is the part where I talk about my projects</h2>
-            <ul>
-                <li>Project 1: PortFoli-Go</li>
-                <li>Project 2: Employee of the Month</li>
-            </ul>
             <h3>Works:</h3>
             <p>Egeplast Website</p>
             <p>This website :0</p>
+
+            <h3>Design Work:</h3>
+            <p>Employee of the Month</p>
+            <p>Personal Work</p>
         </div>
     )
 }
