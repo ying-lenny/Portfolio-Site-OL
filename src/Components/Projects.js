57583dc3782315d07715ada573dc578e8d039dc5
Owen Lenihan <owenlenihan101@gmail.com>
2023-04-27 19:20:54 +0100

    Added rough concept for project tiles in a grid

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,15 +1,43 @@
 import React from "react";
+import '../Styles/Projects.css'
 
 export default function ProjectSection() {
     return (
         <div className="project-section">
-            <h3>Works:</h3>
-            <p>Egeplast Website</p>
-            <p>This website :0</p>
-
-            <h3>Design Work:</h3>
-            <p>Employee of the Month</p>
-            <p>Personal Work</p>
+            <h3>My Work</h3>
+            <div className="project-grid">
+                <div className="tiles">
+                    <div className="project">
+                        <figure className="project-container">
+                            <img src={require('../Assets/PortFoliGo-logo.png')} alt="Port"/>
+                            <figcaption className="container-caption">
+                                <h1>PortFoliGo</h1>
+                                <a className="button is-primary">
+                                    <span>Visit Website</span>
+                                </a>
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div className="project">
+                        <figure className="project-container">
+                            <img src={require('../Assets/PortFoliGo-logo.png')} alt="Port"/>
+                            <figcaption className="container-caption">
+                                <h1>PortFoliGo</h1>
+                                <a></a>
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div className="project">
+                        <figure className="project-container">
+                            <img src={require('../Assets/PortFoliGo-logo.png')} alt="Port"/>
+                            <figcaption className="container-caption">
+                                <h1>PortFoliGo</h1>
+                                <a></a>
+                            </figcaption>
+                        </figure>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
