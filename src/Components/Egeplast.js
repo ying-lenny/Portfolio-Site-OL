090ded6be50571f662cde911a5f85b8ec7caac96
Owen Lenihan <owenlenihan101@gmail.com>
2023-07-27 15:15:51 +0100

    Added images, basic text and css to egeplast page

--- src/Components/Egeplast.js
+++ src/Components/Egeplast.js
@@ -1,9 +1,34 @@
 import React from "react";
+import EgeplastHome from '../Assets/EgeplastHome.png'
+import EgeplastMenu from '../Assets/EgeplastMenu.png'
+import EgeplastPipe from '../Assets/EgeplastPipe.png'
 
 export default function Egeplast() {
   return (
-    <div>
-      <h1>What's Good homeboy?</h1>
+    <div className="egeplast-page">
+      <h1>Egeplast Ireland Website redesign</h1>
+      <div className="egeplast-page-body">
+        <div>
+          <img src={EgeplastHome} alt="Egeplast Homepage"/>
+          <img src={EgeplastMenu} alt="Egeplast Menu"/>
+          <img src={EgeplastPipe} alt="Egeplast Pipe"/>
+        </div>
+
+        <p>
+          During my work placement at college, I was working with Egeplast Ireland Ltd, 
+          a local supplier of welding equipment and training, to create a new website to 
+          replace their outdated one which was not designed with mobile views in mind.
+
+          The website was created using Wordpress and Elementor, with a combination 
+          of CSS and HTML over the span of my 3 month work placement,
+          taking input and feedback about the layout and presentation of necesary 
+          information to the site like certificates and pipe fitting dimensions.
+
+          The site from the perspective of phone screen was also an important consideration
+          as typically the type of person to use the site could be either on-site somewhere
+          from their phone which needed to be kept in mind through-out development.
+        </p>
+      </div>
     </div>
   )
 }
--- src/Components/PFG.js
+++ src/Components/PFG.js
@@ -3,7 +3,10 @@ import React from "react";
 export default function PFG() {
   return (
     <div>
-      <h1>What's Good homeboy!?</h1>
+      <h1>PortFoliGo</h1>
+      <p>
+        PortfoliGo was my final year project during my time at college
+      </p>
     </div>
   )
 }
