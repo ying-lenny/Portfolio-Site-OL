ebe6c10509d5baee76a3e5838b75c4aa90e13038
Owen Lenihan <owenlenihan101@gmail.com>
2024-03-25 23:50:13 +0000

    Update ProjectPage.js

--- src/Pages/ProjectPage.js
+++ src/Pages/ProjectPage.js
@@ -3,7 +3,11 @@ import React from "react";
 function ProjectPage() {
 	return (
 		<div>
-			<h1>What's good</h1>
+			<h1>My Interests</h1>
+				<h2>Typesetting</h2>
+				<h2>3D Modelling</h2>
+				<h2>Photoshop</h2>
+				<h2>Others</h2>
 		</div>
 	)
 }
