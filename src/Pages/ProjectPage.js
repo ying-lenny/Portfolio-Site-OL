d732d0c98d50bffcb00a5868d33207e8c9a8df4e
Owen Lenihan <owenlenihan101@gmail.com>
2024-03-30 23:55:13 +0000

    Starting work on interest page

--- src/Pages/ProjectPage.js
+++ src/Pages/ProjectPage.js
@@ -1,13 +1,17 @@
 import React from "react";
+import Interest from "../Assets/InterestTemplate.png"
 
 function ProjectPage() {
 	return (
-		<div>
+		<div className="interest-gallery">
 			<h1>My Interests</h1>
+			<div className="interest-gallery-content">
+				<img src={Interest} alt="Interest Template"/>
 				<h2>Typesetting</h2>
 				<h2>3D Modelling</h2>
 				<h2>Photoshop</h2>
 				<h2>Others</h2>
+			</div>
 		</div>
 	)
 }
